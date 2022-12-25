f741b829072b8df4cd75ad9212f5f5f247c34c01
weeaa <108926252+allieRobotics@users.noreply.github.com>
2022-12-25 01:05:15 +0100

    🥲 Login not working

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
 	"log"
 	"net/http"
-	"net/http/cookiejar"
 	"net/url"
 	"os"
 	"strings"
@@ -19,9 +19,9 @@ import (
 // 🚨 If you need help using the monitor, you can reach out to me on Discord at weeaa#4144 (363975551393988620) 🚨
 
 // Email42, Password42, etc... must be modified!
-const Email42 = "xavierniel@free.fr"                                                  // ☎️
-const Password42 = "d9babcc8b57b6a7455b5220fe8346de7e2db06df29ba58c855ab5241e49b3bbb" // ;)
-const DiscordID = "363975551393988620"                                                //weeaa#4144 on Discord
+const Email42 = "xavierniel@free.fr"   // ☎️
+const Password42 = "Hello, 世界"       // ;)
+const DiscordID = "363975551393988620" //weeaa#4144 on Discord
 const DiscordWebhook = "https://discord.com/api/webhooks/..."
 
 // OperatingSystem and UserAgent let you customize headers (you can leave it as it is)
@@ -46,23 +46,22 @@ type RequestSettings struct {
 type Cookies struct {
 	CSRF                        string
 	AdmissionsSessionProduction string
+	MkraStck                    string
 }
 
-// keywords will be used to detect if a Piscine 🏊 is open
+// keywords will be used to detect when a Piscine 🏊 is open
+// you are free to modify them!
 var keywords = []string{
 	"July",
 	"June",
 	"August",
 	"March",
 	"April",
+	"Juillet",
+	"Juin",
+	"Août",
+	"Mars",
 }
-
-var (
-	jar, _ = cookiejar.New(nil)
-	client = http.Client{
-		Jar: jar,
-	}
-)
 
 var (
 	invalidCredentials         = errors.New("ERR Invalid Credentials/Unknown")
@@ -123,18 +122,21 @@ func main() {
 			},
 		}
 		
-		resp, err := client.Do(&req)
+		resp, err := http.DefaultClient.Do(&req)
 		if err != nil {
 			task.defaultSleep(err)
 		}
 		
 		body, _ := io.ReadAll(resp.Body)
+		log.Println(string(body))
 		
 		if resp.StatusCode != 200 {
 			task.defaultSleep(fmt.Sprintf("ERR Unknown Fetching Piscine Availability [%v]", resp.Status))
 			continue
-		} else if strings.Contains(string(body), "42 Paris | Connexion") {
+		} else if strings.Contains(string(body), "42 à Paris | Connexion") {
+			log.Println("Re-logging In...")
 			needLogin = true
+			time.Sleep(10 * time.Minute)
 			continue
 		}
 		
@@ -197,7 +199,7 @@ func (t *User) login42Paris() (err error) {
 		},
 	}
 	
-	resp, err := client.Do(&req)
+	resp, err := http.DefaultClient.Do(&req)
 	if err != nil {
 		return err
 	}
@@ -206,7 +208,7 @@ func (t *User) login42Paris() (err error) {
 	for _, c := range cookies {
 		if c.Name == "_admissions_session_production" {
 			t.Cookies.AdmissionsSessionProduction = c.Value
-			log.Println("Got AdmissionsSessionProduction Cookie")
+			log.Printf("Got AdmissionsSessionProduction Cookie [%v]", c.Value)
 		}
 		
 	}
@@ -237,9 +239,17 @@ func (t *User) login42Paris() (err error) {
 		},
 	}
 	
-	resp, err = client.Do(&req)
+	resp, err = http.DefaultClient.Do(&req)
 	if err != nil {
 		return err
+	}
+	
+	cookies = resp.Cookies()
+	for _, c := range cookies {
+		if c.Name == "_mkra_stck" {
+			t.Cookies.MkraStck = c.Value
+			log.Printf("Got MkraStck Cookie [%v]", c.Value)
+		}
 	}
 	
 	body, _ := io.ReadAll(resp.Body)
@@ -253,54 +263,64 @@ func (t *User) login42Paris() (err error) {
 	if t.Cookies.CSRF == "" {
 		return unableGetAuthenticityToken
 	} else {
-		log.Println("Got Authenticity Token")
+		log.Printf("Got Authenticity Token [%v]", t.Cookies.CSRF)
 	}
 	
 	resp.Body.Close()
 	
+	// pr voir si ça fonctionne mais ça bloque tjrs
+	t.processData()
+	
 	//sign in
-	uri, _ = url.Parse("https://admissions.42.fr/users/sign_in")
+	signInURL, _ := url.Parse("https://admissions.42.fr/users/sign_in")
 	
+	// faudra sûrement remodifier sans les caractères spéciaux et retirer t.processData()
 	params := url.Values{
-		"utf8":               {"✓"},
+		"utf8":               {"%E2%9C%93"},
 		"authenticity_token": {t.Cookies.CSRF},
-		"user[email]":        {t.Email},
-		"user[password]":     {t.Password},
+		"user%5Bemail%5B":    {t.Email},
+		"user%5Bpassword%5B": {t.Password},
 	}
-	
-	payload := strings.NewReader(params.Encode())
+	// si modifié changer en strings.NewReader(params.Encode())
+	payload := bytes.NewBufferString(params.Encode())
 	
 	req = http.Request{
-		Method: http.MethodPost,
-		URL:    uri,
-		Body:   io.NopCloser(payload),
+		Method:        http.MethodPost,
+		URL:           signInURL,
+		Body:          io.NopCloser(payload),
+		Host:          "admissions.42.fr",
+		ContentLength: int64(payload.Len()),
 		Header: http.Header{
-			"authority":                 {"admissions.42.fr"},
-			"accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9"},
-			"accept-language":           {"fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7"},
+			"accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"},
+			"accept-encoding":           {"gzip, deflate, br"},
+			"accept-language":           {"en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7"},
 			"cache-control":             {"max-age=0"},
+			"connection":                {"keep-alive"},
 			"content-type":              {"application/x-www-form-urlencoded"},
-			"cookie":                    {"locale=fr; _admissions_session_production=" + t.Cookies.AdmissionsSessionProduction},
-			"origin":                    {"https://admissions.42.fr/"},
+			"origin":                    {"https://admissions.42.fr"},
 			"referer":                   {"https://admissions.42.fr/users/sign_in"},
-			"sec-ch-ua":                 {"\"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Google Chrome\";v=\"108"},
-			"sec-ch-ua-mobile":          {"?0"},
-			"sec-ch-ua-platform":        {t.Settings.OS},
+			"cookie":                    {"locale=fr; _admissions_session_production=" + t.Cookies.AdmissionsSessionProduction},
 			"sec-fetch-dest":            {"document"},
 			"sec-fetch-mode":            {"navigate"},
 			"sec-fetch-site":            {"same-origin"},
 			"sec-fetch-user":            {"?1"},
 			"upgrade-insecure-requests": {"1"},
 			"user-agent":                {t.Settings.UA},
+			"sec-ch-ua":                 {"\"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Google Chrome\";v=\"108"},
+			"sec-ch-ua-mobile":          {"?0"},
+			"sec-ch-ua-platform":        {t.Settings.OS},
 		},
 	}
 	
-	resp, err = client.Do(&req)
+	resp, err = http.DefaultClient.Do(&req)
 	if err != nil {
 		return err
 	}
 	
+	log.Println(resp.Header)
+	
 	resp.Body.Close()
+	body, _ = io.ReadAll(resp.Body)
 	
 	switch resp.StatusCode {
 	case 200, 302:
@@ -353,7 +373,7 @@ func (t *User) sendDiscordNotification() {
 }
 
 func (t *User) safeExit(msg any) {
-	fmt.Println(msg)
+	log.Println(msg)
 	time.Sleep(time.Duration(t.DefaultSleep) * time.Second)
 	os.Exit(0)
 }
@@ -361,4 +381,25 @@ func (t *User) safeExit(msg any) {
 func (t *User) defaultSleep(msg any) {
 	log.Println(msg)
 	time.Sleep(time.Duration(t.DefaultSleep) * time.Second)
+}
+
+func (t *User) processData() {
+	userdata := []string{t.Email, t.Password, t.Cookies.CSRF, t.Cookies.AdmissionsSessionProduction, t.Cookies.MkraStck}
+	
+	for _, value := range userdata {
+		if strings.Contains(value, "@") {
+			value = strings.ReplaceAll(value, "@", "%40")
+		}
+		if strings.Contains(value, "+") {
+			value = strings.ReplaceAll(value, "+", "%2B")
+		}
+		if strings.Contains(value, "=") {
+			value = strings.ReplaceAll(value, "=", "%3D")
+		}
+		
+		log.Println("processed", value)
+		
+		// add more if needed
+	}
+	
 }
